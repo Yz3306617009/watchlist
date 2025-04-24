1d271d6f198b5a20bbb83ded29426e7aabf2da16
yqh <1810508596@qq.com>
2025-04-24 11:44:26 +0800

    完成了程序的主要功能：添加、编辑和删除电影条目

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, flash, redirect
+# Flask 会在请求触发后把请求信息放到 request 对象里，你可以从 flask 包导入它....它在请求触发时才会包含数据，所以你只能在视图函数内部调用它。它包含请求相关的所有信息，比如请求的路径（request.path）、请求的方法（request.method）、表单数据（request.form）、查询字符串（request.args）等等
+# flash提示消息发送到模板展示给用户
+# 重定向响应是一类特殊的响应，它会返回一个新的 URL，浏览器在接受到这样的响应后会向这个新 URL 再次发起一个新的请求。Flask 提供了 redirect() 函数来快捷生成这种响应，传入重定向的目标 URL 作为参数，比如 redirect('http://helloflask.com')。
 from markupsafe import escape
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy  #使用 SQLAlchemy 操作数据库。（ORM，即对象关系映射）
@@ -10,6 +13,9 @@ app = Flask(__name__)
 写入配置的语句一般会放到扩展类实例化语句之前。'''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')#写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# flash() 函数在内部会把消息存储到 Flask 提供的 session 对象里。session 用来在请求间存储数据，它会把数据签名后存储到浏览器的 Cookie 中，所以我们需要设置签名所需的密钥：
+# 这个密钥的值在开发时可以随便设置。基于安全的考虑，在部署时应该设置为随机字符，且不应该明文写在代码里， 在部署章节会详细介绍。
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 
 # 创建数据库模型，，模型类中的属性称之为字段，，模型类中进行各种操作来代替写 SQL 语句
@@ -71,13 +77,59 @@ def page_not_found(e):
 
 @app.route('/index')
 @app.route('/home')
-@app.route('/')#路由的作用就是将这些 URL 与处理这些请求的函数关联起来。  一个视图函数也可以绑定多个 URL，这通过附加多个装饰器实现
+# 两种方法的请求有不同的处理逻辑：对于 GET 请求，返回渲染后的页面；对于 POST 请求，则获取提交的表单数据并保存。为了在函数内加以区分，我们添加一个 if 判断：
+@app.route('/', methods=['GET', 'POST'])#路由的作用就是将这些 URL 与处理这些请求的函数关联起来。  一个视图函数也可以绑定多个 URL，这通过附加多个装饰器实现
 def index():
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('无效的输入')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('成功创建')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
     user = User.query.first()  # 读取用户记录，，，有上下文处理函数就不用这句了
     movies = Movie.query.all()  # 读取所有电影记录
     # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
     return render_template('/index.html', movies=movies)#render_template() 函数可以把模板渲染出来，必须传入的参数为模板文件名（相对于 templates 根目录的文件路径），这里即 'index.html'。为了让模板正确渲染，我们还要把模板内部使用的变量通过关键字参数传入这个函数
 
+
+# 创建一个用于显示编辑页面和处理编辑表单提交请求的视图函数：
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+# 删除表单
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('删除成功')
+    return redirect(url_for('index'))  # 重定向回主页
 
 @app.route('/user/<name>')
 def user_page(name):
